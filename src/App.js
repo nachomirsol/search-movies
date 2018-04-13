d74aaab657a4eb622fc9ab0add6deaa6f3ed3bf8
nachomirsol <nachomirsol@gmail.com>
2018-04-13 08:52:47 +0200

    react app movie search

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title> Search Movies</Title>
+        <div className='SearchForm-wrapper'>
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {
+          this.state.results.length === 0
+            ? <p>Sin resultados</p>
+            : <p>Con resultados</p>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
